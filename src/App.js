3c7ab4ea9edbf91323e8f17a8aef071860f86f71
otsang3 <ohhtsang@gmail.com>
2021-07-04 13:14:56 +0100

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './actions/index';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const counter = useSelector((state) => state);
+    const dispatch = useDispatch();
+    const handleClick = (e) => {
+        const value = e.target.name;
+        let action;
+        switch (value) {
+            case 'INCREASE':
+                action = increment();
+                break;
+            case 'DECREASE':
+                action = decrement();
+                break;
+            case 'RESET':
+                action = reset();
+                break;
+            default:
+                return;
+        }
+        dispatch(action);
+    };
+    return (
+        <div>
+            <h1>Counter : {counter}</h1>
+            <button name="INCREASE" onClick={handleClick}>
+                Increase
+            </button>
+            <button name="DECREASE" onClick={handleClick}>
+                Decrease
+            </button>
+            <button name="RESET" onClick={handleClick}>
+                Reset
+            </button>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import counterReducer from './reducers/counter';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
